f63faaa3338af783ac40deb22e7196d7898d052a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-05-08 15:55:54 +0000

    test: output, send
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"testing"
 	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/x/exp/teatest"
 )
 
@@ -31,4 +33,19 @@ func TestFinalModel(t *testing.T) {
 	if m.start.After(time.Now().Add(-1 * time.Second)) {
 		t.Errorf("m.start should be more than 1 second ago: %s", m.start)
 	}
+}
+
+func TestOuput(t *testing.T) {
+	tm := teatest.NewTestModel(t, initialModel(10*time.Second), teatest.WithInitialTermSize(300, 100))
+
+	teatest.WaitFor(t, tm.Output(), func(bts []byte) bool {
+		return bytes.Contains(bts, []byte("sleeping 8s"))
+	}, teatest.WithCheckInterval(time.Millisecond*100), teatest.WithDuration(time.Second*3))
+
+	tm.Send(tea.KeyMsg{
+		Type:  tea.KeyRunes,
+		Runes: []rune("q"),
+	})
+
+	tm.WaitFinished(t, teatest.WithFinalTimeout(time.Second))
 }
