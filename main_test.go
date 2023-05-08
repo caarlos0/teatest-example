d474afa44676a3485623ce6b6966ca78fbdf4829
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-05-08 15:55:44 +0000

    test: final model
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -17,3 +17,18 @@ func TestFullOutput(t *testing.T) {
 	}
 	teatest.RequireEqualOutput(t, out)
 }
+
+func TestFinalModel(t *testing.T) {
+	tm := teatest.NewTestModel(t, initialModel(time.Second), teatest.WithInitialTermSize(300, 100))
+	fm := tm.FinalModel(t, teatest.WithFinalTimeout(time.Second*2))
+	m, ok := fm.(model)
+	if !ok {
+		t.Fatalf("final model have the wrong type: %T", fm)
+	}
+	if m.duration != time.Second {
+		t.Errorf("m.duration != 1s: %s", m.duration)
+	}
+	if m.start.After(time.Now().Add(-1 * time.Second)) {
+		t.Errorf("m.start should be more than 1 second ago: %s", m.start)
+	}
+}
